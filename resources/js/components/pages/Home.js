fbe9e1c10d709c3fc73bf3215b2e140e5348aa46
Wojtek <wojciechrzoska@gmail.com>
2021-04-27 19:33:39 +0200

    home page

---
+++ resources/js/components/pages/Home.js
@@ -0,0 +1,15 @@
+import '../WelcomeSection.css';
+import WelcomeSection from "../WelcomeSection";
+import CardsSection from "../CardsSection";
+import Footer from '../Footer';
+function Home() {
+    return(
+        <>
+            <WelcomeSection/>
+            <CardsSection/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
